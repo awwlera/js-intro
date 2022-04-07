7159683308cf11deee9efa2f27e9e38ba954aee0
Valeriia Dobrovolskaya <dobrovolskaya.valeriia@gmail.com>
2022-04-07 19:27:42 +0300

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+let userName = "Valeriia";
+let userSurname = "Dobrovolskaya";
+console.log(userName + " " + userSurname);
+
+alert(`Hello, ${userName} ${userSurname}!`);
+
+let myCity = confirm("Are you in Kyiv?");
+alert(myCity);
